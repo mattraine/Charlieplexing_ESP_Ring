cd3d50917647969285ae6a015f6d2f9605a9a027
Matt Raine <mattraine95@gmail.com>
2021-01-02 22:22:03 +0000

    Added ability to set ring percentage via http request and strobe

--- src/CharliePlexedRing.cpp
+++ src/CharliePlexedRing.cpp
@@ -10,18 +10,21 @@
 #include <NTPClient.h>
 #include <Ticker.h>
 #include <WiFiManager.h>
+#include <string.h>
 
 #define USE_SERIAL Serial
 
 unsigned int interval = 200;
-const int max_on_leds = 2;
-int on_leds[max_on_leds];
+volatile unsigned int max_on_leds = 2;
+volatile int on_leds[number_of_leds];
+volatile bool strobe = false;
+const int tmr1_write_count = 10000;
 
 ESP8266WebServer server(80);
 WebSocketsServer webSocket = WebSocketsServer(81);
 
 Ticker timer;
-volatile unsigned int p_onLeds = 0; 
+unsigned int p_onLeds = 0; 
 
 const long utcOffsetInSeconds = 0;
 char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
@@ -48,15 +51,31 @@ void ledsOn(int leds[], int arraySize)
   }
 }
 
+void ledsOn(std::vector<int> leds)
+{
+  for(int l : leds)
+  {
+    ledOn(l);
+  }
+}
+
 // ISR to Fire when Timer is triggered
+volatile int interupt_count=0;
 void ICACHE_RAM_ATTR onTime() {
+  interupt_count++;
+  if(strobe && (interupt_count>100)){
+    ledOn(number_of_leds-1);
+    if(interupt_count>200)interupt_count=0;
+    // Re-Arm the timer as using TIM_SINGLE
+    timer1_write(tmr1_write_count); //  5 ticks per us
+    return;
+  }
   ledOn(on_leds[p_onLeds]);
   p_onLeds++;
   if(p_onLeds+1>max_on_leds) p_onLeds = 0;
 	// Re-Arm the timer as using TIM_SINGLE
-	timer1_write(50000); //  5 ticks per us
+	timer1_write(tmr1_write_count); //  5 ticks per us
 }
-
 
 void sequence_all()
 {
@@ -73,8 +92,9 @@ void displayTime()
 {
   timeClient.update();
   int hours = timeClient.getHours();
-  if(hours>11)hours-=12;  // convert from 24h format to 12h. 
   int mins = timeClient.getMinutes();
+  if(mins>30)hours++;
+  if(hours>11)hours-=12;  // convert from 24h format to 12h. 
   mins = (int) ceil(mins/5.0);
   if(mins>11)mins-=12;
   // int leds[2] = {hours, mins};
@@ -83,6 +103,7 @@ void displayTime()
   // ISR uses these values to switch between charlieplexing leds. 
   on_leds[0] = hours;
   on_leds[1] = mins;
+  max_on_leds = 2;
 }
 
 void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t length) {
@@ -111,6 +132,42 @@ void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t length
           break;
   }
 
+}
+
+void display_percentage(int pc)
+{
+  if(pc>100)pc=100;
+  if(pc<0)pc=0;
+  // use std::vector to change array size. 
+  max_on_leds = (int)floor((pc/100.0) * 12);
+  // on_leds_v.clear();
+  if(max_on_leds == 0)
+  {
+    max_on_leds=1; // to avoid index error at runtime
+    on_leds[0]=number_of_leds-1; // last value configured for LEDS_OFF
+  }else {
+    for (int i=0; i<max_on_leds; i++)
+    {
+      on_leds[i]=i;
+      // on_leds_v.push_back(i);
+    }
+  }
+}
+
+void handleArgs()
+{
+  String message = "Heya!!";
+  if (server.arg("PC")=="")
+  {
+    message = "Please send correct argv: \"PC\"";
+  }else if (server.arg("PC")=="HOME"){
+    display_percentage(100);
+    strobe = true;
+  }else{
+    display_percentage(std::atoi(server.arg("PC").c_str()));
+    strobe = false;
+  }
+  server.send(200, "text/plain", message);
 }
 
 
@@ -176,10 +233,12 @@ void setup() {
 
   // handle index
   server.on("/", []() {
-      // send index.html
-      server.sendHeader("Content-Encoding", "gzip");
-      server.send(200, "text/html", index_html_gz, index_html_gz_len);
+    // send index.html
+    server.sendHeader("Content-Encoding", "gzip");
+    server.send(200, "text/html", index_html_gz, index_html_gz_len);
   });
+
+  server.on("/setPC", handleArgs);
 
   server.begin();
 
@@ -188,6 +247,8 @@ void setup() {
   webSocket.onEvent(webSocketEvent);
 
   timeClient.begin();
+
+  display_percentage(90);
 
   //Initialize Ticker every 0.5s
 	timer1_attachInterrupt(onTime); // Add ISR Function
@@ -200,12 +261,9 @@ void setup() {
 		TIM_SINGLE	0 //on interrupt routine you need to write a new value to start the timer again
 		TIM_LOOP	1 //on interrupt the counter will start with the same value again
 	*/
-	
+
 	// Arm the Timer for our 0.5s Interval
-	timer1_write(2500000); // 2500000 / 5 ticks per us from TIM_DIV16 == 500,000 us interval 
-
-  displayTime();
-
+	timer1_write(tmr1_write_count);
 }
 
 unsigned long timerStart = millis();
@@ -213,7 +271,9 @@ void loop() {
   ArduinoOTA.handle();
   server.handleClient();
   webSocket.loop();
+
   // sequence_all();
+
   if((millis()-timerStart)>60000) 
   {
     displayTime();
